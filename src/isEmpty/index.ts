f9c2a1b46f3c6de7f125968c93e8eb286c9a8086
xiaoli <18959261995@163.com>
2023-07-17 19:00:23 +0800

    change isEmpty

--- src/isEmpty/index.ts
+++ src/isEmpty/index.ts
@@ -1,11 +1,21 @@
+
 /**
- * 判断空对象
- * @param {Object} source 需要验证的对象
+ * 判断数据是否是空对象/空数组/空字符串
+ * @param {String | Array<T> | object} source 需要验证的对象
  * @returns {Boolean} 返回布尔值
  * @example
  * isEmpty({}) => true
  * isEmpty({ a: 1 }) => false
+ * 
+ * isEmpty([]) => true
+ * isEmpty([1, 2, 3]) => false
+ * 
+ * isEmpty('') => true
+ * isEmpty('123') => false
  */
-export function isEmpty(source: Object) {
-  return Reflect.ownKeys(source).length === 0 && source.constructor === Object;
+ export function isEmpty<T>(source: String | Array<T> | object) {
+  if(source.constructor === Object) return Reflect.ownKeys(source).length === 0 
+  return (source as  String | Array<T>).length === 0
 }
+
+
